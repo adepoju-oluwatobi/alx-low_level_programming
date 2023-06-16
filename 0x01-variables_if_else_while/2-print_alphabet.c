0564b204fda77e73cac59fd3cf17626854c19b28
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-16 16:18:46 -0700

    corrected

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,10 @@ int main(void)
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0, i < 26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
