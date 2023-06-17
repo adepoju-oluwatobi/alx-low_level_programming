3b74969e0b98a01af2806e9694adff53b9b2a759
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-16 20:00:34 -0700

    advance 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,13 +7,13 @@ int main(void)
 {
         int n, m, o;
 
-        for (n = 48; n <= 56; n++)
+        for (n = 48; n <= 58; n++)
         {
-                for (m = 49; m <= 57; m++)
+                for (m = 49; m <= 87; m++)
                 {
 			for (o = 50; o <= 58; o++)
 			{
-			       	if (o > m && m  > n)
+			       	if (o > m&&m  > n)
                         	{
                                 	putchar(n);
                                 	putchar(m);
