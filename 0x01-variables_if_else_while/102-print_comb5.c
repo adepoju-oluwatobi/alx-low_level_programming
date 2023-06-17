52961071e1132c4d6639dd4720612f5f22a8612e
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-17 03:56:43 -0700

    corected

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,16 +7,22 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 100; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b < 100; b++)
 		{
-			putchar(a + '0');
-			putchar(b + '0');
-			if (a != 9 || b != 9)
+			if (a < b)
 			{
-				putchar(',');
+				putchar((a/10) + '0');
+				putchar((a%10) + '0');
 				putchar(' ');
+				putchar((b/10) '0');
+				putchar((b%10) '0');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
