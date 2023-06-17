ce4f42f66ce97795333f80b6a47df17871466aaa
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-17 04:00:24 -0700

    corrected

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ int main(void)
 		{
 			if (a < b)
 			{
-				putchar((a/10) + '0');
-				putchar((a%10) + '0');
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
 				putchar(' ');
-				putchar((b/10) + '0');
-				putchar((b%10) + '0');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
 				if (a != 98 || b != 99)
 				{
 					putchar(',');
