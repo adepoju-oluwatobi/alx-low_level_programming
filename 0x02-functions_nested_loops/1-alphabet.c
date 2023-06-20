2a9763e98c33e0d5b8391374083f922f21fb4502
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-20 05:38:10 -0700

    task 01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - program to print alphabet in lowercase
+ *
+ * Return - Always 0
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int a;
+
+	for(a = 0; a < 26; a++)
+	{
+		_putchar(alph[a]);
+	}
+	_putchar('\n');
+	return (0);
+}
