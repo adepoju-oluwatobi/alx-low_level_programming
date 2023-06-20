2bd90528391da97462e81d5f4bda66f43ab911e7
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-20 12:04:17 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isLower - a function that checks for lowercase character.
+ *
+ * Return - 0 Always
+ */
+int _isLower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
