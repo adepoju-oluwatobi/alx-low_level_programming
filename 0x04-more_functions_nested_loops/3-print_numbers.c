eb7da407dcce66b9c0cc4c6a6cd81f722804ddf6
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-22 16:27:43 +0100

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print number from (0 to 9)
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar("%d", a);
+	}
+	_putchar('\n');
+}
