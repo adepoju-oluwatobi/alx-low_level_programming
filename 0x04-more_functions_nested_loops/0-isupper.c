7e1951280bd15f6c890746f96fd2213700631a6e
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-22 14:11:17 +0100

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - check the code.
  * @c: character gotten from prototype.
@@ -7,11 +8,7 @@
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return(1);
-	}
-	else
-	{
-		return(0);
-	}
+
+	return(0);
 }
