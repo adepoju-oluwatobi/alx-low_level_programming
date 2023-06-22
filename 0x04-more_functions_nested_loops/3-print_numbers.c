015ed8e22aff98417e21aac8e42d05ae1fb8b3b2
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-22 17:01:18 +0100

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from (0 to 9),
+ * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
  */
 
 void print_numbers(void)
 {
-	char num[10] = '01234567890';
 	int a;
 
 	for (a = 0; a < 10; a++)
 	{
-		_putchar(num[a]);
+		_putchar(a + '0');
 	}
+
 	_putchar('\n');
 }
