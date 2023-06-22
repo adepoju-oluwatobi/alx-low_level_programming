17421caf829720a45f120860561e83202c71ddbc
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-22 21:44:17 +0100

    finished mandatory task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle aligned right, using '#'
+ * @size: Size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	a = 0;
+	b = size - 1;
+	while (a < size)
+	{
+		b = size - 1 - c;
+		c = a + 1;
+		while (a > 0)
+		{
+			_putchar(' ');
+			b--;
+		}
+		while (c > 0)
+		{
+			_putchar('#');
+			c--;
+		}
+		_putchar('\n');
+		a++;
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
