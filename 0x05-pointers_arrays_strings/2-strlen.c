cd4ac5ff95d9a1464adbdab4fb7fe89813a79996
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-26 14:14:08 +0100

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of string
+ * @s: string to work with
+ * return: the lenght of the string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	return (a);
+}
