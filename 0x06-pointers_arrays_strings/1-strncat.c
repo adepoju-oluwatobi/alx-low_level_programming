f7e2c786258075d85e6ed5b4319d2c9558eaef2b
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-29 14:18:50 +0100

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,9 +10,9 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int a, b;
 
-	for (a = 0; dest[a] = '\n'; a++)
+	for (a = 0; dest[a] = '\0'; a++)
 	{
-		for (b = 0; src[b] = '\n' && n > 0; b++; n--; a++)
+		for (b = 0; src[b] = '\0' && n > 0; b++; n--; a++)
 		{
 			dest[a] = src[b];
 		}
