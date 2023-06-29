88a5ca2b06feda4f147d9682fbd000efae679373
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-29 22:21:03 +0100

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,14 +6,14 @@
  */
 char *string_toupper(char *c)
 {
-        int a;
+	int a;
 
-        for (a = 0; c[a] != '\0'; a++)
-        {
-                if (c[a] > 96 && c[a] < 123)
-                {
-                        c[a] -= 32;
-                }
-        }
-        return (c);
+	for (a = 0; c[a] != '\0'; a++)
+	{
+		if (c[a] > 96 && c[a] < 123)
+		{
+			c[a] -= 32;
+		}
+	}
+	return (c);
 }
