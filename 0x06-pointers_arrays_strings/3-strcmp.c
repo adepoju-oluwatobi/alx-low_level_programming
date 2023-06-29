4b09916a91c5b7fe690df466b97d31145f062871
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-29 19:07:41 +0100

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings.
+ * @dest: string varible to compare
+ * @src: string variable to compre
+ * @n: variable to copy compared strings to
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+			return (s1[a] - s2[a]);
+	}
+	return (0);
+}
