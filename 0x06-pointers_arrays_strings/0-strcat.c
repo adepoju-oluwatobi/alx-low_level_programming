9e680ebbd0ddc709effdef8bfe2b15814378d681
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-29 07:39:07 +0100

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strcat - a function that concatenates two strings.
+ * Return: Returns a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return dest;
+}
