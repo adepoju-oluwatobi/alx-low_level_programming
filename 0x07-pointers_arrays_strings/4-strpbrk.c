9a5ac2dd1e21b38077f0f62a9ebb0f325f40fb3a
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-03 16:07:46 +0100

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,16 +3,20 @@
  * _strpbrk - locates the first occurrence in the string `s`
  * @s: variable containing complete string
  * @accept: variable to search
- * Return:  a pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found
+ * Return:  a pointer to the byte in `s`
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
+	int a;
+	int b;
+	int c;
+
+	a = 0;
+	c = 0;
 
 	while (s[a] != '\0')
 	{
+		b = 0;
 		while (accept[b] != '\0')
 		{
 			if (s[a] == accept[b])
