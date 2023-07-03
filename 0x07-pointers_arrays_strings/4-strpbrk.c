3527dedcc6dc11ae9724e2b7ff15af9de12ea117
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-03 20:45:21 +0100

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,8 +4,6 @@
  * @s: string
  * @accept: string to match
  * Return: Pointer to the byte in `s` that matches
- * one of the bytes in `accept`
- * or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
