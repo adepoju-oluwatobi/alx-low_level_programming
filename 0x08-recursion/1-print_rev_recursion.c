a2cf7882aa8ea3a267ca461e09b6156bcd08750b
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-05 19:00:38 +0100

    solution to task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,12 +8,8 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		return (0);
-	}
-	else
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
