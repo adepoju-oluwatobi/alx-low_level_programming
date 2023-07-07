3577aa5d217651ec9ad9fc58626b2a955c58ebbd
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-07 22:32:01 +0100

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the number passed into it
+ * @argc: number of arguments passed
+ * @argv: array of argument passed
+ * Return: 0 always
+ */
+int main(int argc, *char argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
