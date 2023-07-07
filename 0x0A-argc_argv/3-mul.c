eab70b69667aba01fd5ae502506d90f2d653883e
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-07 23:02:38 +0100

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,17 @@ int main(int argc, char *argv)
 {
 	int a, b, mul;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		mul = a * b;
+		
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
 
-		printf("%d\n", mul);
-	}
+	printf("%d\n", mul);
+
 	return (0);
 }
