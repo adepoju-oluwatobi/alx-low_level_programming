ffe9b9486ae0c373c32d91d621c182a08124091a
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-17 21:23:02 +0100

    solution to task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - a dogs info
+ * @name: Name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ * Description: No description
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif
