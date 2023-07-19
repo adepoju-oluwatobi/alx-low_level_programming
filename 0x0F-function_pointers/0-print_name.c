8dce66fef8b0f4fb0ad55c5947a60c1b58106a5e
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-19 21:04:18 +0100

    solution to task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointer.h"
+/**
+ * print_name - a function that prints a name
+ * @name: name to print
+ * @f: pointer to funtion print_name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(Tobi);
+}
