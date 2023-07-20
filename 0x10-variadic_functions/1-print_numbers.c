05a5ee847069928ec37f70a96bea4d7161cbe9cb
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-20 21:13:15 +0100

    solution to task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,8 +1,8 @@
 #include "variadic_functions.h"
 /**
  * print_numbers - a function that prints numbers, followed by a new line.
+ * @n: number variable to print
  * @separator: character to separate numbers
- * @n: numbers to print
  * Return: Always 0
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
