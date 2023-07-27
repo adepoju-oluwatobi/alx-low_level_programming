f94a2d2e45444976a8a2cb70da63dbe31bbef610
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-27 21:47:43 +0100

    solution to advance task 1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * print_first - a function that prints some strings to stdout
  * Return: success
  */
-void print_first(void) __attribute__((constructor))
+void first(void) __attribute__((constructor))
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
