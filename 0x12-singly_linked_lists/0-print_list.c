2b181ab9cbf5995a98647b7911167d09990863b6
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-27 12:03:00 +0100

    solution to task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,14 +11,11 @@ size_t print_list(const list_t *h)
 	while (h !- NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[%d] %s\n", 0, "(nil)");
-		}
 		else
-		{
 			/* print lenght of list and value */
 			printf("[%d] %s\n", h->len, h->str);
-		}
+
 		/* allocate the add next address to h */
 		h = h->next;
 		i++;
