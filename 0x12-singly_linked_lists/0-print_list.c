e3fd573b7e309540e791f37c31ae304e48d4812b
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-27 10:18:36 +0100

    solution to task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_list - function that prints all the elements.
+ * @h: linked list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h !- NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			/* print lenght of list and value */
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		/* allocate the add next address to h */
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
