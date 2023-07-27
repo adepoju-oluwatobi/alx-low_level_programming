0b2c180f396959f1734d600326ec180e23717f9e
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-27 13:33:37 +0100

    solution to task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,25 +1,26 @@
 #include "lists.h"
 /**
  * add_node - function that add a new node
+ * @head: head of the linked list
  * @str: string to be duplicated
  * Return: the address of the new element.
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	size_t i = 0;
+	size_t i;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
+
 	new_node->str = strdup(str);
 
-	for (i; str[i]; i++)
-		new_node->len = i;
-		new_node->next = *head;
-		*head = new_node;
+	for (i = 0; str[i]; i++)
+
+	new_node->len = i;
+	new_node->next = *head;
+	*head = new_node;
 
 	return (*head);
 }
