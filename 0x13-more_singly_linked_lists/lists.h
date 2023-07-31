1c4bb7bc1fe9b5b3ee7afc1b47f24ef98be98189
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-31 19:46:09 +0100

    solution to task 0

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "node.c"
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+
+#endif
