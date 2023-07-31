d1bcaac0c58f7630533eb37f61107b3097415c68
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-31 20:24:55 +0100

    solution to task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_nodeint - a function that adds a new node at the beginning of a listint_t list.
+ * add_nodeint - a function that adds a new node
  * @head: head of node
  * @n: integer data
  * Return: address of the newly added node
