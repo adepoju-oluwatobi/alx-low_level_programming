5d3bff06898ae8389965a7ab96e52e1b510c2d55
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-01 18:30:05 +0100

    solved

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 /**
- *
+ * free_listint_safe - a function that frees a listint_t list.
+ * @h: list's head
+ * Return: size of the list freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
