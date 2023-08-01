16b09e1de41c6cb5d61f77926116be05851cff91
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-01 17:45:22 +0100

    solved

--- 0x13-more_singly_linked_lists/new_node.c
+++ 0x13-more_singly_linked_lists/new_node.c
@@ -1,11 +1,1 @@
-#include "lists.h"
-/**
- * struct new_node- singly linked list
- * @p: ptr to nodes
- * @next: points to next node
- */
-typedef struct node
-{
-	void *p;
-	struct node *next;
-} new_node;
+
