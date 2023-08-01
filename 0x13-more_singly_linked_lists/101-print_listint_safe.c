6ee396e614fa2364ecad680d406a15211f47277f
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-01 18:09:09 +0100

    solved

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,64 +1,30 @@
 #include "lists.h"
 /**
- * free_list - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
-void free_list(new_node **head)
-{
-	new_node *temp;
-	new_node *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-/**
- * print_listint_safe - a function that prints a listint_t linked list.
- * @head: the head of node list
+ * size_t print_listint_safe -  a function that prints a listint_t
+ * @head: head of linked list
  * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	listint_t *i = head;
+	listint_t *j = head;
 	size_t count = 0;
-	listint_t *ptr, *new, *add;
 
-	ptr = NULL;
-	while (head != NULL)
+	while (i != NULL && j != NULL && j->next != NULL)
 	{
-		new = malloc(sizeof(new_node));
+		i = i->next;
+		j = j->next->next;
 
-		if (new == NULL)
+		if (i == j)
 			exit(98);
 
-		new->n = (void *)head;
-		new->next = ptr;
-		ptr = new;
+		printf("%d", i->n);
+		if (i-> != NULL)
+			printf("->");
+		else
+			printf("-> NULL\n");
 
-		add = ptr;
-
-		while (add->next !- NULL)
-		{
-			add = add->next;
-			if (head == add->p)
-			{
-				printf("-[%p]%d\n", (void *)head, head->n;
-				free_list(&ptr);
-				return (count);
-			}
-		}
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
 		count++;
 	}
-	free_list(&ptr);
-	return (count);
+	return count;
 }
--- 0x13-more_singly_linked_lists/node.c
+++ 0x13-more_singly_linked_lists/node.c
@@ -11,14 +11,3 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
-
-/**
- * struct new_node- singly linked list
- * @p: ptr to nodes
- * @next: points to next node
- */
-typedef struct node
-{
-        void *p;
-        struct node *next;
-} new_node;
