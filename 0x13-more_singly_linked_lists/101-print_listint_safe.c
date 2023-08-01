3448a2644eef8ede2dec749cf9ab53d09668bda0
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-01 17:43:19 +0100

    solved

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,27 @@
 #include "lists.h"
 /**
+ * free_list - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_list(new_node **head)
+{
+	new_node *temp;
+	new_node *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+/**
  * print_listint_safe - a function that prints a listint_t linked list.
  * @head: the head of node list
  * Return: the number of nodes in the list
@@ -12,7 +34,7 @@ size_t print_listint_safe(const listint_t *head)
 	ptr = NULL;
 	while (head != NULL)
 	{
-		new = malloc(sizeof(listint_t));
+		new = malloc(sizeof(new_node));
 
 		if (new == NULL)
 			exit(98);
@@ -26,12 +48,17 @@ size_t print_listint_safe(const listint_t *head)
 		while (add->next !- NULL)
 		{
 			add = add->next;
-			if (head == add->n)
+			if (head == add->p)
 			{
 				printf("-[%p]%d\n", (void *)head, head->n;
-				free(count);
+				free_list(&ptr);
+				return (count);
 			}
 		}
-		free(ptr);
-		return(count);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		count++;
+	}
+	free_list(&ptr);
+	return (count);
 }
