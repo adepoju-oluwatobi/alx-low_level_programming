24951e937fbb04e4258a8f8bfaa63a71186446ba
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-01 18:22:58 +0100

    solved

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,72 +1,25 @@
 #include "lists.h"
-
 /**
- * free_list - frees a linked list
- * @head: head of a list.
  *
- * Return: no return.
- */
-void free_list(new_node **head)
-{
-	new_node *temp;
-	new_node *current;
-
-	if (head != NULL)
-	{
-		current = *head;
-		while ((temp = current) != NULL)
-		{
-			current = current->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * free_listint_safe - frees a linked list.
- * @h: head of a list.
- *
- * Return: size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nodes = 0;
-	listp_t *ptr, *new, *add;
-	listint_t *current;
+	listint_t *i = *h;
+	size_t count = 0;
 
-	ptr = NULL;
-	while (*h != NULL)
+	while (i != NULL)
 	{
-		new = malloc(sizeof(new_node));
+		listint_t *j = i->next;
 
-		if (new == NULL)
-			exit(98);
+		free(i);
+		count++;
 
-		new->p = (void *)*h;
-		new->next = ptr;
-		ptr = new;
-
-		add = ptr;
-
-		while (add->next != NULL)
+		if (j == *h)
 		{
-			add = add->next;
-			if (*h == add->p)
-			{
-				*h = NULL;
-				free_list(&ptr);
-				return (nodes);
-			}
+			*h = NULL;
+			break;
 		}
-
-		current = *h;
-		*h = (*h)->next;
-		free(current);
-		nodes++;
+		i = j;
 	}
-
-	*h = NULL;
-	free_list(&ptr);
-	return (nodes);
+	return (count);
 }
