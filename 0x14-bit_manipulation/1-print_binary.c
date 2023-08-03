a8534f57dd00645feeb7bb233d0f2b8058a98118
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-03 12:42:58 +0100

    solved

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_binary - function that prints the binary representatio
+ * @n: decimal number to convert
+ * Return: Nothing
+ */
+unsigned long int _exp(unsigned int base, unsigned int exp);
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char stat;
+
+	stat = 0;
+	divisor = exp(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			stat = 1;
+			putchar('1');
+		}
+		else if (state == 1 || divisor == 1)
+		{
+			putchar('0');
+		}
+		divisor >>= 1;
+	}
+}
+/**
+ * _exp - function that calc the exponential value of binary
+ * @base: base number
+ * @exp: exponent
+ * Return: number
+ */
+unsigned long int _exp(unsigned int base, unsigned int exp)
+{
+	unsigned long int number;
+	unsigned int i;
+
+	number = 1;
+	for (i = 1; i <= power; i++)
+		number *= base;
+	return (number);
+}
