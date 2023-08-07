acc2f1f8c50a8c7d12b031fba6ee20c99a9bffe0
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-07 13:42:24 -0700

    solved task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file_desc == -1)
 		return (-1);
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
 		bytes_written = write(file_desc, text_content, strlen(text_content));
 		if (bytes_written == -1)
