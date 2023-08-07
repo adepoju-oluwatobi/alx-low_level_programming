f7418e79ab8edaafacd0b6b36402d15fd209acab
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-07 13:15:38 -0700

    solved task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - function that creates a file
  * @filename: file to create
- * @text_content: Holds the content of the file.
+ * @text_content: Holds the content of the file
  * Return: 1 on success and -1 if filename is null
  */
 int create_file(const char *filename, char *text_content)
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	file_desc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	
+
 	if (file_desc == -1)
 		return (-1);
 
@@ -33,4 +33,3 @@ int create_file(const char *filename, char *text_content)
 	close(file_desc);
 	return (1);
 }
-		
