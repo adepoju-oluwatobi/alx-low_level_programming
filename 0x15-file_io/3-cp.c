2ffddd8973ba832f005968649e823b3d34caa522
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-08 05:46:22 +0100

    solved task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,79 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
-#define BUF_SIZE 1024;
+void check_IO_status(int status, int fd, char *filename, char perm);
 /**
- * main - a program that copies the content of a file to another file.
+ * main - copies the content of one file to the other
  * @argc: argument count
- * @argv: argument value
- * Return: 0
+ * @argv: arguments passed
+ *
+ * Return: 1 on success, exit otherwise
  */
 int main(int argc, char *argv[])
 {
-	const char *file_from;
-	const char *file_to;
-	int fd_from;
-	int fd_to;
-	ssize_t read_from, written_to;
-	char buf[BUF_SIZE];
+	int file_from, file_to, bytes_read = 1024, bytes_written, close_file_from, close_file_to;
+	unsigned int perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	file_from = argv[1];
-	file_to = argv[2];
-
-	fd_from = open(file_from, O_RDONLY);
-
-	if (fd_from == -1)
+	file_from = open(argv[1], O_RDONLY);
+	check_IO_status(file_from, -1, argv[1], 'O');
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, perm);
+	check_IO_status(file_to, -1, argv[2], 'W');
+	while (bytes_read == 1024)
 	{
-		dprintf(STDERR_FILENO, "%s", "Error: Can't read from file %s\n", file_from);
+		bytes_read = read(file_from, buffer, sizeof(buffer));
+		if (bytes_read == -1)
+			check_IO_status(-1, -1, argv[1], 'O');
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written == -1)
+			check_IO_status(-1, -1, argv[2], 'W');
+	}
+	close_file_from = close(file_from);
+	check_IO_status(close_file_from, file_from, NULL, 'C');
+	close_file_to = close(file_to);
+	check_IO_status(close_file_to, file_to, NULL, 'C');
+	return (0);
+}
+
+/**
+ * check_IO_status - checks if a file can be opened or closed
+ * @status: file descriptor of the file to be opened
+ * @filename: name of the file
+ * @perm: closing or opening
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check_IO_status(int status, int fd, char *filename, char perm)
+{
+	if (perm == 'C' && status == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	else if (perm == 'O' && status == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
-
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
-	if (fd_to == -1)
+	else if (perm == 'W' && status == -1)
 	{
-		dprintf(STDERR_FILENO, "%s", "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
 	}
-
-	while ((read_from = read(fd_from, buf, BUF_SIZE)) > 0)
-	{
-		written_to = write(fd_to, buf, read_from);
-
-		if (written_to == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
-	}
-
-	if (read_from == -1)
-	{
-		dprintf(STDERR_FILENO, "%s", "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "%s", "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "%s", "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
-	return (0);
 }
