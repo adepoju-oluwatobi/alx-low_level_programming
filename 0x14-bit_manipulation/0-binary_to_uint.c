6fe318a7ea2be337a6c38d928d47dbac25076eb9
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-31 13:54:27 -0700

    corrected task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,22 +6,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int x;
-	unsigned int num;
+	int i;
+	unsigned int number;
 
-	num = 0;
+	number = 0;
 	if (!b)
 		return (0);
-	for (x = 0; b[x] != '\0'; x++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[x] != '0' && b[x] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-	for (x = 0; b[x] != '\0'; x++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		num <<= 1;
-		if (b[x] == '1')
-			num += 1;
+		number <<= 1;
+		if (b[i] == '1')
+			number += 1;
 	}
-	return (num);
+	return (number);
 }
