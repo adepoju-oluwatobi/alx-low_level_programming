c0109ff91dff30662f08cd2198b7fc86606e4d4a
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-10-16 20:34:03 +0100

    added solutions

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * add - function that sums up two integers
+ * a: first integer parameter
+ * b: second integer parameter
+ *
+ * Return: addition of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * sub - function that sums up two integers
+ * a: first integer parameter
+ * b: second integer parameter
+ *
+ * Return: subtraction of a and b
+ */
+
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
